94e2d0d36eaf00978688d110656cd59cdd48ce50
Michael Swanson <mikeswanson12@msn.com>
2015-06-09 16:21:58 -0400

    wrap angular controller post and delete functions to be called from view

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var path = require('path');
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../views','index.html'));
 });
 
 
